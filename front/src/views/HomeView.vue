7c5b627c1243f404d1d33c2f8ee362a41c9f1930
HYUNBIN_CHO <bluesky3268@gmail.com>
2022-08-08 22:38:05 +0900

    게시글 리스트 화면

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const posts = ref([]);
+
+axios.get("/hyunbennylog-api/posts?page=1&size=5")
+.then((response) =>{
+  response.data.forEach((post : any) => {
+    posts.value.push(post);
+  })
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>
+        {{post.title}}
+      </div>
+      <div>
+        {{post.content}}
+      </div>
+    </li>
+  </ul>
 </template>
+<style>
+li{
+  margin-bottom: 1rem;
+}
+li:last-child {
+  margin-bottom: 0;
+}
+</style>
--- front/src/views/RegistView.vue
+++ front/src/views/RegistView.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const title = ref("");
 const content = ref("");
+
+const router = useRouter();
 
 const register = function(){
   // axios.post("")
   axios.post("/hyunbennylog-api/posts", {
     title: title.value,
     content: content.value
+  })
+      // 글 작성 성공 후 홈으로 이동
+  .then(() => {
+    router.replace({name: "home"}); // 글 작성 후 홈으로 갔을 때 뒤로가기 하면 다시 글 등록화면으로 가지는 것을 막기 위해서 push가 아니라 replace를 주로 사용함
   })
 }
 
