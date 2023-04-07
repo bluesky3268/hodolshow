7de59b543b93af891e3d9c112300eb02a482fc8e
hbcho <bluesky3268@gmail.com>
2023-04-07 17:01:50 +0900

    헤더를 통한 방식이 아닌 많이 쓰는 쿠키를 사용하는 방식으로 변경해 봄.

--- src/main/java/com/hyunbennylog/api/config/AuthResolver.java
+++ src/main/java/com/hyunbennylog/api/config/AuthResolver.java
@@ -5,12 +5,17 @@ import com.hyunbennylog.api.domain.Session;
 import com.hyunbennylog.api.exception.UnAuthorizedException;
 import com.hyunbennylog.api.repository.SessionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -23,8 +28,22 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if(accessToken == null || accessToken.equals("")) throw new UnAuthorizedException();
+//        String accessToken = webRequest.getHeader("Authorization");
+//        if(accessToken == null || accessToken.equals("")) throw new UnAuthorizedException();
+
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (request == null) {
+            log.error("servletRequest is null");
+            throw new UnAuthorizedException();
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies.length == 0) {
+            log.error("cookies arr length is 0");
+            throw new UnAuthorizedException();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         // DB 사용자 확인 작업
         Session findSession = sessionRepository.findByAccessToken(accessToken)
--- src/main/java/com/hyunbennylog/api/controller/AuthController.java
+++ src/main/java/com/hyunbennylog/api/controller/AuthController.java
@@ -1,17 +1,18 @@
 package com.hyunbennylog.api.controller;
 
-import com.hyunbennylog.api.domain.User;
-import com.hyunbennylog.api.exception.InvalidLoginInfoException;
-import com.hyunbennylog.api.exception.InvalidRequestException;
-import com.hyunbennylog.api.repository.UserRepository;
 import com.hyunbennylog.api.request.LoginRequest;
 import com.hyunbennylog.api.response.SessionResponse;
 import com.hyunbennylog.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -21,11 +22,24 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody LoginRequest request) {
+    public ResponseEntity<Object> login(@RequestBody LoginRequest request) {
         // 요청받은 id와 비밀번호(json형식)을 데이터 베이스에서 조회한 후 일치할 경우 토큰 리턴
         log.info("request : {}", request.toString());
         String accessToken = authService.login(request);
-        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("HYUNBENNYLOG_SESSION_ID", accessToken)
+                .domain("localhost") // TODO : 서버 환경에 따라 분리할 필요 있음
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>>> cookie : {}", cookie.toString());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 
 
